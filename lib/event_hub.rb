537abaa868ea8585f484a1711c44420b619c6a87
Bogdan Guban <bogdan@seekingalpha.com>
2023-05-26 18:34:06 +0300

    Initial commit

---
+++ lib/event_hub.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "event_hub/version"
+
+module EventHub
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/event_hub_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe EventHub do
+  it "has a version number" do
+    expect(EventHub::VERSION).not_to be nil
+  end
+end
